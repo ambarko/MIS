168689a02651eba30365e6d325de7ba8dc9a6da3
ambarko <ambarkovm@gmail.com>
2025-01-08 23:26:21 +0100

    lab4

---
+++ lab4/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:lab4/providers/exam_provider.dart';
+import 'package:lab4/screens/calendar_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ExamProvider(),
+      child: MaterialApp(
+        title: 'Lab4: Raspored za polaganje',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: CalendarScreen(),
+      ),
+    );
+  }
+}
